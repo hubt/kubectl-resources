6772f882aa21f2975af822c0da95877d40025e57
Hubert Chen <hubert@chen.net>
2023-05-29 11:07:40 -0700

    further cleanup

--- kubectl-podinfo.go
+++ kubectl-podinfo.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"os/exec"
 	"text/tabwriter"
-	//"strings"
 
 	"gopkg.in/yaml.v2"
 )
@@ -14,43 +13,29 @@ import (
 type PodList struct {
 	APIVersion string `yaml:"apiVersion"`
 	Kind       string `yaml:"kind"`
-	Items      []Pod  `yaml:"items"`
-}
+	Items      []struct {
+		Metadata struct {
+			Name      string `yaml:"name"`
+			Namespace string `yaml:"namespace"`
+		} `yaml:"metadata"`
 
-type Metadata struct {
-	Name      string `yaml:"name"`
-	Namespace string `yaml:"namespace"`
+		Spec struct {
+			Containers []struct {
+				Name      string
+				Resources struct {
+					Limits struct {
+						Memory string `yaml:"memory"`
+						CPU    string `yaml:"cpu"`
+					} `yaml:"limits"`
+					Requests struct {
+						Memory string `yaml:"memory"`
+						CPU    string `yaml:"cpu"`
+					} `yaml:"requests"`
+				}
+			}
+		} `yaml:"spec"`
+	} `yaml:"items"`
 }
-
-type Pod struct {
-	Metadata   Metadata `yaml:"metadata"`
-	Spec       PodSpec  `yaml:"spec"`
-}
-
-type PodSpec struct {
-	Containers []Container
-}
-
-type Container struct {
-	Name      string
-	Resources Resources
-}
-
-type Resources struct {
-	Limits   Limits   `yaml:"limits"`
-	Requests Requests `yaml:"requests"`
-}
-
-type Limits struct {
-	Memory string `yaml:"memory"`
-	CPU    string `yaml:"cpu"`
-}
-
-type Requests struct {
-	Memory string `yaml:"memory"`
-	CPU    string `yaml:"cpu"`
-}
-
 
 func main() {
 	// Execute "kubectl get pods -A -o  yaml" command
@@ -58,6 +43,7 @@ func main() {
 	output, err := cmd.Output()
 	if err != nil {
 		log.Fatalf("Error executing kubectl command: %s", err)
+		os.Exit(1)
 	}
 
 	var yamlMap PodList
@@ -67,16 +53,14 @@ func main() {
 	}
 
 	w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', 0)
-	fmt.Fprintf(w,"NAMESPACE\tPOD\tCONTAINER\tCPU\tMEMORY\n")
+	fmt.Fprintf(w, "NAMESPACE\tPOD\tCONTAINER\tCPU\tMEMORY\n")
 
 	pods := yamlMap
 	for _, pod := range pods.Items {
 		for _, container := range pod.Spec.Containers {
-			fmt.Fprintf(w,"%s\t%s\t", pod.Metadata.Namespace, pod.Metadata.Name)
-			fmt.Fprintf(w,"%s\t", container.Name)
-			fmt.Fprintf(w,"%s/%s\t", container.Resources.Requests.CPU, container.Resources.Limits.CPU)
-			fmt.Fprintf(w,"%s/%s\t", container.Resources.Requests.Memory, container.Resources.Limits.Memory)
-			fmt.Fprintf(w,"\n")
+			fmt.Fprintf(w, "%s\t%s\t%s\t", pod.Metadata.Namespace, pod.Metadata.Name, container.Name)
+			fmt.Fprintf(w, "%s/%s\t", container.Resources.Requests.CPU, container.Resources.Limits.CPU)
+			fmt.Fprintf(w, "%s/%s\n", container.Resources.Requests.Memory, container.Resources.Limits.Memory)
 		}
 	}
 	w.Flush()
