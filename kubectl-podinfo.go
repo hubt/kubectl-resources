7410252970926421b41ae286d5fce5ec1548630f
Hubert Chen <hubert@chen.net>
2023-05-29 13:48:20 -0700

    add kubectl top integration

--- kubectl-podinfo.go
+++ kubectl-podinfo.go
@@ -5,15 +5,14 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 	"text/tabwriter"
 
 	"gopkg.in/yaml.v2"
 )
 
 type PodList struct {
-	APIVersion string `yaml:"apiVersion"`
-	Kind       string `yaml:"kind"`
-	Items      []struct {
+	Items []struct {
 		Metadata struct {
 			Name      string `yaml:"name"`
 			Namespace string `yaml:"namespace"`
@@ -31,6 +30,11 @@ type PodList struct {
 						Memory string `yaml:"memory"`
 						CPU    string `yaml:"cpu"`
 					} `yaml:"requests"`
+					// add this to capture what kubectl top says
+					Utilization struct {
+						Memory string `yaml:"memory"`
+						CPU    string `yaml:"cpu"`
+					}
 				}
 			}
 		} `yaml:"spec"`
@@ -42,26 +46,64 @@ func main() {
 	cmd := exec.Command("kubectl", "get", "pods", "-A", "-o", "yaml")
 	output, err := cmd.Output()
 	if err != nil {
-		log.Fatalf("Error executing kubectl command: %s", err)
+		log.Fatalf("Error executing kubectl get pods command: %s", err)
 		os.Exit(1)
 	}
 
-	var yamlMap PodList
-	err = yaml.Unmarshal(output, &yamlMap)
+	var podList PodList
+	err = yaml.Unmarshal(output, &podList)
 	if err != nil {
 		log.Fatalf("Error parsing YAML: %s", err)
 	}
 
-	w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', 0)
-	fmt.Fprintf(w, "NAMESPACE\tPOD\tCONTAINER\tCPU\tMEMORY\n")
+	parseTop(&podList)
+	w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', tabwriter.Debug)
+	fmt.Fprintf(w, "NAMESPACE\tPOD\tCONTAINER\tCPU:UTIL\tCPU:REQ\tCPU:LIM\tMEM:UTIL\tMEM:REQ\tMEM:LIM\n")
 
-	pods := yamlMap
+	pods := podList
 	for _, pod := range pods.Items {
 		for _, container := range pod.Spec.Containers {
 			fmt.Fprintf(w, "%s\t%s\t%s\t", pod.Metadata.Namespace, pod.Metadata.Name, container.Name)
-			fmt.Fprintf(w, "%s/%s\t", container.Resources.Requests.CPU, container.Resources.Limits.CPU)
-			fmt.Fprintf(w, "%s/%s\n", container.Resources.Requests.Memory, container.Resources.Limits.Memory)
+			fmt.Fprintf(w, "%s\t%s\t%s\t", container.Resources.Utilization.CPU, container.Resources.Requests.CPU, container.Resources.Limits.CPU)
+			fmt.Fprintf(w, "%s\t%s\t%s\n", container.Resources.Utilization.Memory, container.Resources.Requests.Memory, container.Resources.Limits.Memory)
 		}
 	}
 	w.Flush()
+}
+
+func parseTop(podList *PodList) error {
+	cmd := exec.Command("kubectl", "top", "pods", "-A", "--containers", "--no-headers")
+	output, err := cmd.Output()
+	if err != nil {
+		log.Fatalf("Error executing kubectl top command: %s", err)
+		os.Exit(1)
+	}
+
+	for _, line := range strings.Split(string(output), "\n") {
+		s := strings.Fields(line)
+		if len(s) != 5 {
+			continue
+		}
+
+		namespace := s[0]
+		pod := s[1]
+		container := s[2]
+		cpu := s[3]
+		memory := s[4]
+		for i, p := range podList.Items {
+			if p.Metadata.Namespace == namespace && p.Metadata.Name == pod {
+				for j, c := range p.Spec.Containers {
+					if c.Name == container {
+						podList.Items[i].Spec.Containers[j].Resources.Utilization.CPU = cpu
+						podList.Items[i].Spec.Containers[j].Resources.Utilization.Memory = memory
+						continue
+					}
+				}
+				continue
+			}
+		}
+
+	}
+
+	return nil
 }
